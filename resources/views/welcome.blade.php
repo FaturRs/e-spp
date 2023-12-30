41abb9e2b4761649bca1fe9e269a0fda0b2fef7e
FaturRs <faturviagenzjr@gmail.com>
2023-12-30 20:05:22 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+@section('content')
+<div class="main-content">
+ <!-- Header -->
+ <div class="header bg-primary pt-5 pb-7">
+   <div class="container">
+     <div class="header-body">
+       <div class="row align-items-center">
+         <div class="col-lg-6">
+           <div class="pr-5">
+             <h1 class="display-2 text-white font-weight-bold mb-0">E-SPP Online</h1>
+             <h2 class="display-4 text-white font-weight-light">SMA Negeri 1 Kamal</h2>
+             <p class="text-white mt-4">Pembayaran SPP Online Berbasis Web</p>
+             <div class="mt-5">
+               <a href="{{ url('contact')}}" class="btn btn-neutral my-2">E - mail</a>
+               <a href="https://wa.me/628813125679" class="btn btn-success my-2">Whatsapp</a>
+             </div>
+           </div>
+         </div>
+         <div class="col-lg-6">
+           <div class="row pt-5">
+             <div class="col-md-6">
+         </div>
+       </div>
+     </div>
+   </div>
+   <div class="separator separator-bottom separator-skew zindex-100">
+     <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+       <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+     </svg>
+   </div>
+ </div>
+
+ 
+ </div>
+ @endsection
